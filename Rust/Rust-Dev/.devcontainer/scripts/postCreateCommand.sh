65a673060394b319aa893ece21cf559a61f1042a
stephenandary <stephen@jamminmusic.com>
2022-11-14 09:15:35 -0500

    update homebrew install step

--- Rust/Rust-Dev/.devcontainer/scripts/postCreateCommand.sh
+++ Rust/Rust-Dev/.devcontainer/scripts/postCreateCommand.sh
@@ -8,6 +8,10 @@ rustup target add wasm32-wasi
 
 echo "Installing Hombrew"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/vscode/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
 
 echo "Installing NATS CLI"
 brew tap nats-io/nats-tools
